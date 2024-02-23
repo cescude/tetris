21d875052b0dc0a0c5c13168e280d859763af6fc
Chandler Escude <chandler.escude@gmail.com>
2024-02-23 08:52:56 -0500

    Lots of refactoring, probably introduced some bugs :^(

--- tetris.c
+++ tetris.c
@@ -8,20 +8,23 @@
 #include "io.h"			/* Cursor & input functions  */
 #include "pieces.h"		/* Piece definitions */
 
-int WIDTH = 0;
-int HEIGHT = 0;
-
 struct State {
   int x, y, dir;
   enum Piece p, pn;
   int score;
 };
 
-void printLayer(char *buffer, char clear) {
-  for ( int i=0; i<HEIGHT; i++ ) {
-    for ( int j=0; j<WIDTH; j++ ) {
-      if ( buffer[i*WIDTH+j] ) {
-	putchar(buffer[i*WIDTH+j]);
+struct Board {
+  char* background;
+  char* foreground;
+  int width, height;
+};
+
+void printLayer(char *buffer, int width, int height, char clear) {
+  for ( int i=0; i<height; i++ ) {
+    for ( int j=0; j<width; j++ ) {
+      if ( buffer[i*width+j] ) {
+	putchar(buffer[i*width+j]);
       } else if (clear) {
 	putchar(' ');
       } else {
@@ -31,31 +34,26 @@ void printLayer(char *buffer, char clear) {
     putchar('\n');
   }
 
-  cursorUp(HEIGHT);
+  cursorUp(height);
 }
 
-void placePiece(char *buffer, enum Piece p, int rotation, int x, int y, char ch) {
-  int offsets[4] = {0};
-  getOffsets(offsets, p, rotation, WIDTH);
+void printForeground(struct Board *board) {
+  printLayer(board->foreground, board->width, board->height, 0);
+}
 
-  int start = y*WIDTH+x;
-  for ( int i=0; i<4; i++ ) {
-    int offset = start + offsets[i];
-    if ( 0 <= offset && offset < WIDTH*HEIGHT ) {
-      buffer[offset] = ch;
-    }
-  }
+void printBackground(struct Board *board) {
+  printLayer(board->background, board->width, board->height, 1);
 }
 
 /* return 1 if `p` can be placed at this location */
-int testPiece(char *buffer, enum Piece p, int rotation, int x, int y) {
+int testPiece(struct Board *board, enum Piece p, int rotation, int x, int y) {
   int offsets[4] = {0};
-  getOffsets(offsets, p, rotation, WIDTH);
+  getOffsets(offsets, p, rotation, board->width);
 
-  int start = y*WIDTH+x;
+  int start = y*board->width+x;
   for ( int i=0; i<4; i++ ) {
     int offset = start + offsets[i];
-    if ( 0 <= offset && offset < WIDTH*HEIGHT && buffer[offset] ) {
+    if ( 0 <= offset && offset < board->width*board->height && board->background[offset] ) {
       return 0;
     }
   }
@@ -63,103 +61,138 @@ int testPiece(char *buffer, enum Piece p, int rotation, int x, int y) {
   return 1;
 }
 
-int landingPoint(char *background, enum Piece p, int dir, int x, int y) {
+int landingPoint(struct Board *board, struct State *st) {
+  int y = st->y;
+  
   /* go down until we collide */
-  while (testPiece(background, p, dir, x, y)) {
+  while (testPiece(board, st->p, st->dir, st->x, y)) {
     y++;
   }
 
   return y-1; 			/* Need to backup one */
 }
 
-void drawFrame(char *background, struct State *st, char include_ghost) {
-  char layer[10000] = {0};
-  printLayer(background, 1);
-  
-  if ( include_ghost ) {
-    placePiece(layer, st->p, st->dir, st->x, landingPoint(background, st->p, st->dir, st->x, st->y), '.');
+void placePiece(char *buffer, int width, int height, enum Piece p, int rotation, int x, int y, char ch) {
+  int offsets[4] = {0};
+  getOffsets(offsets, p, rotation, width);
+
+  int start = y*width+x;
+  for ( int i=0; i<4; i++ ) {
+    int offset = start + offsets[i];
+    if ( 0 <= offset && offset < width*height ) {
+      buffer[offset] = ch;
+    }
   }
-  placePiece(layer, st->p, st->dir, st->x, st->y, '#');
+}
 
-  printLayer(layer, 0);
+void placeGhost(struct Board *board, struct State *st) {
+  int ghost_point = landingPoint(board, st);
+  placePiece(board->foreground, board->width, board->height, st->p, st->dir, st->x, ghost_point, '.');
+}
 
-  cursorDn(HEIGHT);
+void placePlayer(struct Board *board, struct State *st) {
+  placePiece(board->foreground, board->width, board->height, st->p, st->dir, st->x, st->y, '#');
+}
+
+void stampPlayer(struct Board *board, struct State *st) {
+  int stamp_point = landingPoint(board, st);
+  placePiece(board->background, board->width, board->height, st->p, st->dir, st->x, stamp_point, '#');
+}
+
+void printStats(struct Board *board, struct State *st) {
+  cursorDn(board->height);
   printf("SCORE=%d, NEXT=%c\n", st->score, piece_names[st->pn]);
-  cursorUp(HEIGHT + 1);
+  cursorUp(board->height + 1);
 }
 
-void initBoard(char *layer) {
-  memset(layer, 0, WIDTH*HEIGHT);
-  for (int i=0; i<HEIGHT-1; i++) {
-    layer[i*WIDTH] = layer[i*WIDTH+WIDTH-1] = '|';
+void drawFrame(struct Board *board, struct State *st) {
+  printBackground(board);
+
+  memset(board->foreground, 0, board->width*board->height);
+  
+  placeGhost(board, st);
+  placePlayer(board, st);
+  
+  printForeground(board);
+
+  printStats(board, st);
+}
+
+void initBoard(struct Board *board) {
+  int width = board->width;
+  int height = board->height;
+  
+  memset(board->background, 0, width*height);
+  memset(board->foreground, 0, width*height);
+  
+  for (int i=0; i<height-1; i++) {
+    board->background[i*width] = board->background[i*width+width-1] = '|';
   }
 
-  for (int i=0; i<WIDTH; i++) {
-    layer[(HEIGHT-1)*WIDTH+i] = '-';
+  for (int i=0; i<width; i++) {
+    board->background[(height-1)*width+i] = '-';
   }
 }
 
-int testLine(char *background, int y) {
-  int offset = y * WIDTH;
-  for (int i=0; i<WIDTH; i++) {
-    if ( !background[offset+i] ) {
+int testLine(struct Board *board, int y) {
+  int offset = y * board->width;
+  for (int i=0; i<board->width; i++) {
+    if ( !board->background[offset+i] ) {
       return 0;
     }
   }
   return 1;
 }
 
-void squashLine(char *background, int y) {
+void squashLine(struct Board *board, int y) {
   for (; y>0; y--) {
-    int offset = y * WIDTH;
-    for (int i=0; i<WIDTH; i++) {
-      background[offset+i] = background[offset-WIDTH+i];
+    int offset = y * board->width;
+    for (int i=1; i<board->width-1; i++) {
+      board->background[offset+i] = board->background[offset-board->width+i];
     }
   }
-  for (int i=1; i<WIDTH-2; i++) {
-    background[i] = 0;
+  for (int i=1; i<board->width-1; i++) {
+    board->background[i] = 0;
   }
 }
 
-int countAllLines(char *background) {
+int countAllLines(struct Board *board) {
   int num_lines = 0;
-  for ( int i=0; i<HEIGHT-1; i++ ) {
-    num_lines += testLine(background, i);
+  for ( int i=0; i<board->height-1; i++ ) {
+    num_lines += testLine(board, i);
   }
   return num_lines;
 }
 
-void squashAllLines(char *background) {
-  for (int i=0; i<HEIGHT-1; i++) {
-    if (testLine(background, i)) {
-      squashLine(background, i);
+void squashAllLines(struct Board *board) {
+  for (int i=0; i<board->height-1; i++) {
+    if (testLine(board, i)) {
+      squashLine(board, i);
     }
   }
 }
 
 /* return 0 if there can't be another round... */
-int nextRound(char *background, struct State *st) {
-  st->y = landingPoint(background, st->p, st->dir, st->x, st->y);
-  
-  placePiece(background, st->p, st->dir, st->x, st->y, '#');
+int nextRound(struct Board *board, struct State *st) {
 
-  /* Check to see if we've cleared any lines */
-  int num_lines = countAllLines(background);
+  stampPlayer(board, st);
+
+  int num_lines = countAllLines(board);
 
   if ( num_lines ) {
-    drawFrame(background, st, 0);
+    printBackground(board);
     getEvents(num_lines);	/* Just need a simple delay */
-    squashAllLines(background);
+    squashAllLines(board);
   }
 
   st->p = st->pn;
   while ( (st->pn = rand()%NUM_PIECES) == st->p ); /* Don't do two-in-a-rows */
   st->dir = 0;
-  st->x = WIDTH/2-1;
+  st->x = board->width/2-1;
   st->y = 0;
   st->score += num_lines;
 
-  return testPiece(background, st->p, st->dir, st->x, st->y);
+  return testPiece(board, st->p, st->dir, st->x, st->y);
 }
 
 int computeDelay(int score) {
@@ -168,19 +201,25 @@ int computeDelay(int score) {
 }
 
 int main(int argc, char** argv) {
-  WIDTH = argc > 1 ? atoi(argv[1]) : 12;
-  HEIGHT = argc > 2 ? atoi(argv[2]) : 11;
+  int WIDTH = argc > 1 ? atoi(argv[1]) : 12;
+  int HEIGHT = argc > 2 ? atoi(argv[2]) : 11;
 
   srand(time(0));
 
-  char *board = (char*)malloc(sizeof(char)*WIDTH*HEIGHT);
-
-  if ( board ) {
-    initBoard(board);
-  } else {
-    printf( "Too large!\n" );
+  char* buffer = (char*)malloc(sizeof(char)*WIDTH*2*HEIGHT);
+  if ( !buffer ) {
+    printf("Too large!\n");
     return 1;
   }
+
+  struct Board board = {
+    .background = buffer,
+    .foreground = buffer + WIDTH*HEIGHT,
+    .width = WIDTH,
+    .height = HEIGHT,
+  };
+
+  initBoard(&board);
 
   struct State st = {
     .x = WIDTH/2-1,
@@ -191,7 +230,7 @@ int main(int argc, char** argv) {
     .pn = rand()%NUM_PIECES,
   };
 
-  drawFrame(board, &st, 1);
+  drawFrame(&board, &st);
 
   cursorOn(0);
     
@@ -211,7 +250,7 @@ int main(int argc, char** argv) {
     int end_round = 0;
 
     if ( (evt & E_TIMER) || (evt & E_SDROP) ) {
-      if ( testPiece(board, st.p, st.dir, st.x, st.y+1) ) {
+      if ( testPiece(&board, st.p, st.dir, st.x, st.y+1) ) {
 	st.y++;
       } else {
 	end_round = 1;
@@ -220,37 +259,37 @@ int main(int argc, char** argv) {
     
     if ( evt & E_HDROP ) {
       /* Find the lowest spot and force a new round */
-      st.y = landingPoint(board, st.p, st.dir, st.x, st.y);
+      st.y = landingPoint(&board, &st);
       end_round = 1;
     }
 
 
     if ( end_round ) {
-      if ( !nextRound(board, &st) ) {
+      if ( !nextRound(&board, &st) ) {
 	break;
       }
-    } else if ( testPiece(board, st.p, nd, nx, st.y) ) {
+    } else if ( testPiece(&board, st.p, nd, nx, st.y) ) {
       st.dir = nd;
       st.x = nx;
     } 
     
-    drawFrame(board, &st, 1);
+    drawFrame(&board, &st);
   }
 
-  char overlay[10000] = {0};
-  memcpy(overlay, board, WIDTH*HEIGHT);
+  memcpy(board.foreground, board.background, WIDTH*HEIGHT);
   
   for (int limit=HEIGHT-2; limit>=0; limit-=1) {
     for (int i=HEIGHT-2; i>=limit; i--) {
-      memset(overlay + i*WIDTH + 1, 'X', WIDTH - 2);
+      memset(board.foreground + i*WIDTH + 1, 'X', WIDTH - 2);
     }
-    printLayer(overlay, 1);
+    printBackground(&board);
+    printForeground(&board);
     getEvents(1);
   }
 
-  drawFrame(board, &st, 1);
+  printBackground(&board);
 
-  free(board);
+  free(buffer);
 
   cursorDn(HEIGHT + 2);
   cursorOn(1);
